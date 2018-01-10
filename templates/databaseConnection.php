88bed88e109b1b813efef8745870715a727e2019
MasseyIsaako <lasiisaako@gmail.com>
2018-01-10 15:27:10 +1300

    changed 1 parameter to get DB connection working

--- templates/databaseConnection.php
+++ templates/databaseConnection.php
@@ -1,5 +1,5 @@
 <?php
-	$dbc = mysqli_connect('localhost', 'root', '', 'bookLibrary');
+	$dbc = mysqli_connect('localhost', 'root', 'root', 'bookLibrary');
 	if(!$dbc){
 		die("Server error, database not responding");
 	}
